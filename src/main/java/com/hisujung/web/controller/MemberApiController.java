5e1aea6790a33c52f57dc6d7827781e16393d370
Leeyoujin00 <cymn6032@gmail.com>
2023-08-20 00:20:39 +0900

    .

--- src/main/java/com/hisujung/web/controller/MemberApiController.java
+++ src/main/java/com/hisujung/web/controller/MemberApiController.java
@@ -44,7 +44,7 @@ public class MemberApiController {
 
     @PostMapping("/join/mailConfirm")
     @ResponseBody
-    public String mailConfirm(@RequestBody String email) throws Exception {
+    public String mailConfirm(@RequestParam String email) throws Exception {
         log.info(email);
         String code = mailSender.send(email);
         log.info("인증코드 : " + code);
